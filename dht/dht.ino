e20197c5586b05ae77ec47a90fd79078db39960e
Ujjwal <itsujjwalkasera@gmail.com>
2023-03-19 15:45:53 +0530

    dht code updated

--- dht/dht.ino
+++ dht/dht.ino
@@ -1,14 +1,35 @@
- #include <DHT.h>
- DHT dht2(2,DHT11);  
- void setup()  
- {  
-  Serial.begin(9600);  
- }  
- void loop()  
- {  
-   Serial.println("Temperature in C:");  
-   Serial.println((dht2.readTemperature( )));  
-   Serial.println("Humidity in C:");  
-   Serial.println((dht2.readHumidity()));  
-   delay(1000);  
- }  
+#include <DHT.h>
+#define DHTPIN 2 
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("Measuring Temperature & Humidity.........."));
+  delay(1000);
+  dht.begin();
+} 
+void loop(){
+float hum = dht.readHumidity();  
+float tempC = dht.readTemperature();
+float tempF = dht.readTemperature(true);
+// To check whether data is collected or not
+if (isnan(hum) || isnan(tempC)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+}
+float hifrah = dht.computeHeatIndex(tempF, hum);
+float hicel = dht.computeHeatIndex(tempC, hum, false);
+Serial.print(F(" Humidity: "));
+Serial.print(hum);
+Serial.print(F("%  Temperature: "));
+Serial.print(tempC);
+Serial.print(F("°C "));
+Serial.print(tempF);
+Serial.println(F("°F"));
+Serial.print("Temperature Feels like: ");
+Serial.print(hicel);
+Serial.print(F("°C "));
+Serial.print(hifrah);
+Serial.println(F("°F"));
+delay(1000);
+}
